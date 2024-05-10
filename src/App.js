0c42e8dd14689828dc7e24482a4930f8f8133c87
Matheus Victorio <matheusvic2016@gmail.com>
2024-05-10 01:05:44 -0300

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import  Image from './assets/profissao-programador.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='back-ground'></div>
+      <div className='chat-container'>
+        
+        <div className='chat-contacts'>
+          <div className='chat-options'></div>
+          <div className='chat-item'>
+              <img src={Image} className='image-profile' alt=''/>
+              <div className='title-chat-container'>
+                <span className='title-message'>Networking Profissão Programador</span>
+                <span className='last-message'>Matheus: Bom dia!</span>
+              </div>
+          </div>
+        </div>
+
+
+      <div className='chat-messages'>
+        <div className='chat-options'>
+            <div className='chat-item'>
+              <img src={Image} className='image-profile' alt=''/>
+              <div className='title-chat-container'>
+                <span className='title-message'>Networking Profissão Programador</span>
+                <span className='last-message'>Matheus, Gabriela, Manuella, Kauan.</span>
+              </div>
+          </div>
+        </div>  
+
+        <div className='chat-messages-area'>
+          
+        </div>
+        <div className='chat-imput-area'>
+          
+        </div>
+      </div>      
+      
+      </div>
     </div>
   );
 }
